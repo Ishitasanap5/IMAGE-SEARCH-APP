edd3c40836d1b072c520a0be36718f84fbfa71da
Ishita <ishitasanap5@gmail.com>
2026-03-28 15:20:37 +0530

    feat: create reusable ImageCard component

---
+++ src/components/ImageCard.jsx
@@ -0,0 +1,31 @@
+export default function ImageCard({ image }) {
+  return (
+    <div className="mb-4 break-inside-avoid group relative cursor-pointer">
+      
+      {/* Image */}
+      <img
+        src={image.webformatURL}
+        alt={image.tags}
+        className="rounded-xl w-full transition duration-300 group-hover:brightness-75"
+      />
+
+      {/* Overlay */}
+      <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition flex flex-col justify-between p-3">
+        
+        {/* Top right (save button style) */}
+        <div className="flex justify-end">
+          <button className="bg-red-500 text-white px-3 py-1 rounded-full text-xs">
+            Save
+          </button>
+        </div>
+
+        {/* Bottom info */}
+        <div className="text-white text-xs">
+          <p className="font-semibold">{image.user}</p>
+          <p>❤️ {image.likes} &nbsp; 👁️ {image.views}</p>
+        </div>
+
+      </div>
+    </div>
+  );
+}
